d7cd592ce4a97979ab0179054477f1013544af44
rohan <rshah04@gmail.com>
2024-03-04 17:25:41 -0500

    Create Globals.h

---
+++ virtual_pet_24/Globals.h
@@ -0,0 +1,13 @@
+// Globals.h
+// Storing global variables
+
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+extern bool selected[4];
+extern int NUTRITION;
+extern int HAPPINESS;
+extern int ENERGY;
+
+
+#endif
